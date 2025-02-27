6ff4f7606d5623fb207c0341cd4f5c1c385902ca
Ceri Binding <ceri.binding@southwales.ac.uk>
2025-02-27 10:18:17 +0000

    amend base in wite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/steleto-web/',
+  base: 'steleto-web',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
